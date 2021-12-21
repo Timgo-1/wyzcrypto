0bb92a10f3e5ba72b88a036d0cb0a7953e1773c7
Timgo-1 <timgodesigns@gmail.com>
2021-12-21 19:51:36 +0300

    wyzcrypto

---
+++ README.md
@@ -0,0 +1,8 @@
+# WyzCrypto - Explore the World of Cryptocurrency
+
+![WyzCrypto]()
+
+## Introduction
+This is a code repository for WyzCrypto cryptocurrency app. I used React and multiple APIs powered by https://rapidapi.com.
+
+
---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar, Affix } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
+
+const Exchanges = () => {
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+      <Affix>
+        <Row>
+          <Row span={6}>Exchanges</Row>
+          <Row span={6}>24h Trade Volume</Row>
+          <Row span={6}>Markets</Row>
+          <Row span={6}>Change</Row>
+        </Row>
+      </Affix>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24}>
+            <Collapse>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                header={(
+                  <Col key={exchange.id}>
+                    <Col>
+                      <Text><strong>{exchange.rank}.</strong></Text>
+                      <Avatar className="exchange-image" src={exchange.iconUrl} />
+                      <Text><strong>{exchange.name}</strong></Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange.volume)}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                  </Col>
+                  )}
+              >
+                {HTMLReactParser(exchange.description || '')}
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
